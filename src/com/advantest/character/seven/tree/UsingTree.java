99552d1f658a55f2e8b74aea23fa187ab52de7f7
zhanglin2018 <893330264@qq.com>
2019-05-28 18:25:45 +0800

    materials modified

--- src/com/advantest/character/seven/tree/UsingTree.java
+++ src/com/advantest/character/seven/tree/UsingTree.java
@@ -1,6 +1,5 @@
 package com.advantest.character.seven.tree;
 
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Shell;
